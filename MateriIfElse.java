639995e4c080451208d5208be3a8ac7e3ad3e616
nisriinadyanp <136248612+nisriinadyanp@users.noreply.github.com>
2023-09-22 15:43:29 +0700

    Add files via upload

---
+++ MateriIfElse.java
@@ -0,0 +1,129 @@
+package Pert2IfElse;
+
+public class MateriIfElse {
+    public static void main(String[] args) {
+        // // SELEKSI KONDISI
+        // // Ada 3 jenis seleksi kondisi 
+        // // 1. Conditional Assignment
+        // // 2. IF-ELSEIF-ELSE
+        // // 3. SWITCH-CASE
+
+        // // IF - ELSE - ELSEIF
+
+        // // IF-ELSE 
+        // int a = 7;
+        // int b = 5;
+        // if (a == b) {
+        //     System.out.println("a sama dengan b");
+        // } else {
+        //     System.out.println("a tidak sama dengan b");
+        // }
+
+        // // == -> sama dengan
+        // // != -> tidak sama dengan
+        // // >  -> lebih dari
+        // // >= -> lebih dari sama dengan
+        // // <  -> kurang dari
+        // // <= -> kurang dari sama dengan     
+
+        // int c = 6;
+        // int d = 3;
+        // if (c > d) {
+        //     System.out.println("");
+        // } else {
+        //     System.out.println("d lebih besar daripada c");
+        // }
+
+        // // IF -ELSE IF - ELSE 
+        // int jam = 14;
+        // if (jam < 12) {
+        //     System.out.println("Pagi");
+        // } else if (jam < 16) {
+        //     System.out.println("Siang");
+        // } else if (jam < 18) {
+        //     System.out.println("Sore");
+        // } else {
+        //     System.out.println("Malang");
+        // }
+
+        // int umur = 10; 
+        // if (umur < 2) {
+        //     System.out.println("Balita");
+        // } else if (umur < 10) {
+        //     System.out.println("Anak kecil");
+        // } else if (umur < 18) {
+        //     System.out.println("Remaja");
+        // } else if (umur < 60) {
+        //     System.out.println("Dewasa");
+        // } else {
+        //     System.out.println("Lansia");
+        // }
+
+        // String nama = "Alya";
+        // if(nama == "Eka") {
+        //     System.out.println("Nama sama");
+        // } else {
+        //     System.out.println("Nama beda!");
+        // }
+
+        // String nama2 = "Dina";
+        // if (nama2.equals("Dina")) {
+        //     System.out.println("Nama sama");
+        // } else {
+        //     System.out.println("Nama beda");
+        // }
+
+        // String nama3 = "Ayu";
+        // if (nama3.equalsIgnoreCase("AYU")) {
+        //     System.out.println("Nama sama");
+        // } else {
+        //     System.out.println("Nama beda");
+        // }
+
+        // OPERATOR AND & OR
+        // AND (dan) -> pakai &&
+        // OR (atau) -> pakai ||
+
+        // String namaDepan = "New";
+        // String namaBelakang = "Jeans";
+
+        // if (namaDepan.equals("New") && namaBelakang.equals("Jeans")) {
+        //     System.out.println("Ini NewJeans");
+        // } else {
+        //     System.out.println("Nama salah");
+        // }
+
+        // int angka1 = 10;
+        // int angka2 = 19;
+
+        // if (angka1 == 5 || angka2 <30) {
+        //     System.out.println("IF dijalankan");
+        // } else {
+        //     System.out.println("ELSE dijalankan");
+        // }
+
+        // // Conditional Assignment
+        // int a = 10;
+        // int b = 20;
+        // String x = (a<b) ? "a kurang dari b" : "b kurang dari a";
+        // System.out.println(x);
+        // //lat
+        // String nama4 = "Andi";
+        // System.out.println((nama4.equals("Jamal")) ? "X" : "Y");
+
+        // System.out.println(((b/2) == 10) ? "Nol" : "Satu");
+
+        // // NESTED IF
+        // int umur = 20;
+        // if (umur < 17) {
+        //     System.out.println("Kamu masih di bawah umur");
+        // } else {
+        //     if (umur < 22) {
+        //         System.out.println("Kamu lagi kuliah");
+        //     } else {
+        //         System.out.println("Kamu sudah lulus aamiin");
+        //     }
+        // }
+
+    }
+}
