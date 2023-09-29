17ff665df1b355b33b56da92de4796a0ff211c50
nisriinadyanp <136248612+nisriinadyanp@users.noreply.github.com>
2023-09-29 09:38:37 +0700

    Add files via upload

---
+++ Materi3SwitchCase.java
@@ -0,0 +1,82 @@
+package Pert3SwitchCase;
+
+public class Materi3SwitchCase {
+    public static void main(String[] args) {
+        //SWITCH CASE
+        // switch (kondisi) {
+        //     case 1 :
+        //     break;
+        //     case 2 : 
+        //     break;
+        // }
+
+        // String nama = "Caca";
+
+        // switch (nama) {
+        //     case "Andi" :
+        //         System.out.println("Nama kamu Andi");
+        //         break;
+        //     case "Boni" :
+        //         System.out.println("Nama kamu Boni");
+        //         break;
+        //     case "Caca" :
+        //         System.out.println("Nama kamu caca");
+        //         break;
+        //     case "Didi" :
+        //         System.out.println("Nama kamu Didi");
+        //         break;
+        //     case "Eli" :
+        //         System.out.println("Nama kamu Eli");
+        //         break;
+        //     default : 
+        //         break;
+        // }
+
+        // LENGTH, CHARAT, DAN SUBSTRING
+        // String nama2 = "Nisriin";
+        // String nama3 = "Dyan";
+        // String nama4 = "Nisriina Dyan";
+
+        // // Length = berapa huruf, hasil dalam bentuk int
+        // // namavariabel.length();
+        // int panjangNama2 = nama2.length();
+        // System.out.println(panjangNama2);
+
+        // System.out.println(nama3.length());
+
+        // System.out.println(nama4.length());
+
+        // CharAt = untuk melihat huruf di index tertentu
+        // Index = nomor yang menunjukkan urutan, istilah gampangnya kayak, huruf ke berapa
+        // Di Java, index ini dimulai dari 0
+    
+        // String nama5 = "NISRIINA";
+        // // N I S R I I N A
+        // // 0 1 2 3 4 5 6 7
+        
+        // System.out.println(nama5.length());
+        // // CharAt hasil dalam bentuk Char
+        // char huruf = nama5.charAt(2);
+        // System.out.println(huruf);
+
+        // SUBSTRING = mengambil beberapa huruf dalam suatu kata/kalimat
+        // namavariabel.substring(mulai index ke berapa, berhenti di index ke berapa)
+        
+        // String nama6 = "NISRIINA DYAN";
+
+        // N I S R I I N A   D Y  A  N
+        // 0 1 2 3 4 5 6 7 8 9 10 11 12
+
+        // //System.out.println(nama6.substring(0, 7));
+        // System.out.println(nama6.substring(9, 13));
+
+        // //System.out.println(nama6.substring(9, 13));
+
+        
+        
+
+
+
+
+    }
+}
