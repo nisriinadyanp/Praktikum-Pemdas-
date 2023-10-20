f8269e0bdd07006dfa2e2f1e6795c9c4c8f35e5d
nisriinadyanp <136248612+nisriinadyanp@users.noreply.github.com>
2023-10-20 10:30:59 +0700

    Add files via upload

---
+++ Perulangan1.java
@@ -0,0 +1,99 @@
+package PertPerulangan;
+
+import java.util.*;
+
+public class Perulangan1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        //MATERI PERULANGAN
+        
+        //INCREMENT 
+        // //increment mudahnya berarti +1
+        // int aa = 0;
+        // aa++;
+        // // aa = 0 + 1 = 1
+        // System.out.println(aa);
+        // aa++;
+        // // aa = 1 + 1 = 2
+        // System.out.println(aa);
+
+        //DECREMENT 
+        // // decrement mudahnya berarti -1
+        // int bb = 10;
+        // bb--;
+        // // bb = 10 - 1 = 9
+        // System.out.println(bb);
+        // // bb = 9 - 1 = 8
+        // bb--;
+        // System.out.println(bb);
+
+        // FOR Loop
+        // Digunakan jika kita sudah mengetahui jumlah perulangan
+        
+        // for (int i = 0; i>10; i++) {
+        //     System.out.println("Nilai i=" + i);
+        //     System.out.println("Ini adalah loop ke-" + (i+1));
+        //     System.out.println("=================");
+        //     //System.out.println("Semoga sehat selalu");
+        // }
+
+        // String namaa = "Safira";
+        // for (int j = 0; j<namaa.length(); j++) {
+        //     System.out.println(namaa.charAt(j));
+        // }
+
+        // WHILE Loop
+        // Digunakan jika tidak tahu berapa kali perulangan
+
+        // int index = 0;
+        // while (index<10) {
+        //     System.out.println("Index bernilai " + index);
+        //     System.out.println("Ini perulangan ke-" + (index+1));
+        //     System.out.println("===============================");
+        //     index++;
+        // }
+
+        // System.out.println("tes");
+        // boolean masukLoop = true;
+        // while (masukLoop) {
+        //     System.out.println("tess");
+        //     String inputan = input.nextLine();
+        //     switch (inputan) {
+        //         case "MAKAN" :
+        //         System.out.println("Kamu harus makan!");
+        //         break;
+        //         case "MINUM" :
+        //         System.out.println("Kamu harus minum!");
+        //         break;
+        //         case "TIDUR" :
+        //         System.out.println("Kamu harus tidur!");
+        //         break;
+        //         case "EXIT" :
+        //         System.out.println("Penulisan perintah selesai");
+        //         masukLoop = true;
+        //         break;
+        //     }
+        // }
+
+        
+        // BREAK AND CONTINUE
+        // for (int g = 0; g < 10; g++) {
+        //     if (g == 8) {
+        //         break;
+        //     } else if (g % 2 == 0) {
+        //         System.out.println("Perulangan ke-" + g);
+        //     } else {
+        //         continue;
+        //     }
+        // }
+        
+
+
+
+
+
+
+
+        
+    }
+}
