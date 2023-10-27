064ff0d443482dc3048e3c06170f8d7e0bacb16d
nisriinadyanp <136248612+nisriinadyanp@users.noreply.github.com>
2023-10-27 18:54:40 +0700

    Add files via upload

---
+++ Perulangan2.java
@@ -0,0 +1,57 @@
+package PertPerulangan;
+
+import java.util.*;
+
+public class Perulangan2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        // BREAK AND CONTINUE
+        // for (int g = 0; g < 10; g++) {
+        //     if (g == 8) {
+        //         break;
+        //     } else if (g % 2 == 0) {
+        //         System.out.println("Perulangan ke-" + g);
+        //     } else {
+        //         continue;
+        //     }
+        // }
+
+        // DO-WHILE
+        // sebenernya sama aja sama while, cuma beda di peletakannya aja
+        // int a = 5;
+        // do {
+        //     System.out.println("a = " + a);
+        //     a++;
+        // } while (a < 8);
+
+        // // bedanya, kalo do while bakal ngejalanin programnya minimal 1 kali walaupun kondisi bernilai salah
+        // int b = 5;
+        // do {
+        //     System.out.println("b = " + b);
+        // } while (b>10);
+
+        // NESTED LOOP
+        // for (int t = 0; t<5; t++) {
+        //     System.out.println("FOR1, t = " + t);
+        //     System.out.println("--------------");
+        //     for (int j = 0; j<5; j++) {
+        //         System.out.println("FOR2, j = " + j);
+        //     }
+        //     System.out.println("==============");
+        // }
+
+        // for (int n = 0; n<5; n++) {
+        //     for (int m = 0; m<5; m++) {
+        //         System.out.print("* ");
+        //         if (n == m) {
+        //             break;
+        //         }
+        //     }
+        //     System.out.println("\n");
+        // }
+
+
+
+    }
+}
